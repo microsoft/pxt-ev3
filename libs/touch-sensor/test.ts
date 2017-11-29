f22edac84d228332c3ab39d9e413b35bcead0b03
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-28 16:11:15 -0800

    moving touch stuff into separate projects

---
+++ libs/touch-sensor/test.ts
@@ -0,0 +1,8 @@
+sensors.touchSensor1.onEvent(TouchSensorEvent.Pressed, function () {
+})
+sensors.touchSensor2.onEvent(TouchSensorEvent.Bumped, function () {
+})
+sensors.touchSensor3.onEvent(TouchSensorEvent.Released, function () {
+})
+sensors.touchSensor4.isPressed();
+sensors.touchSensor4.wasPressed();
