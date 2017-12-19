1ab7ae6cfac6f3b27fbd1867951b6ab0149f4865
Sam El-Husseini <samelh@microsoft.com>
2017-12-19 14:57:28 -0800

    minor PR feedback

--- sim/state/light.ts
+++ sim/state/light.ts
@@ -14,7 +14,9 @@ namespace pxsim.output {
     export function setLights(pattern: number) {
         const brickState = ev3board().getBrickNode();
         const lightState = brickState.lightState;
-        lightState.lightPattern = pattern;
-        brickState.setChangedState();
+        if (lightState.lightPattern != pattern) {
+            lightState.lightPattern = pattern;
+            brickState.setChangedState();
+        }
     }
 }
