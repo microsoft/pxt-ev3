7d044672141b5fb16bc1f6e5e1e04760949417bf
Sam El-Husseini <samelh@microsoft.com>
2018-01-04 15:54:18 -0800

    Handle the case where multiple motors or multiple sensors are connected to the same port.

--- sim/state/input.ts
+++ sim/state/input.ts
@@ -1,10 +1,17 @@
 
+
+import lf = pxsim.localization.lf;
+
 namespace pxsim.motors {
 
     export function __motorUsed(port: number, large: boolean) {
         //console.log("MOTOR INIT " + port);
-        ev3board().motorUsed(port, large);
-        runtime.queueDisplayUpdate();
+        if (!ev3board().hasMotor(port)) {
+            ev3board().motorUsed(port, large);
+            runtime.queueDisplayUpdate();
+        } else {
+            U.userError(`${lf("Multiple motors are connected to Port")} ${String.fromCharCode('A'.charCodeAt(0) + ev3board().motorMap[port])}`);
+        }
     }
 }
 
@@ -12,7 +19,11 @@ namespace pxsim.sensors {
 
     export function __sensorUsed(port: number, type: number) {
         //console.log("SENSOR INIT " + port + ", type: " + type);
-        const sensor = ev3board().getSensor(port, type);
-        runtime.queueDisplayUpdate();
+        if (!ev3board().hasSensor(port)) {
+            const sensor = ev3board().getSensor(port, type);
+            runtime.queueDisplayUpdate();
+        } else {
+            U.userError(`${lf("Multiple sensors are connected to Port")} ${port + 1}`);
+        }
     }
 }
