3aaad52da4dabfdc6fb515479f2c789247e5d61d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-11 15:57:03 -0800

    send console to storage

--- libs/storage/storage.ts
+++ libs/storage/storage.ts
@@ -1,2 +1,15 @@
 namespace storage {
+    // automatically send console output to temp storage
+    console.addListener(function(line) {
+        const fn = "console.txt";
+        const mxs = 65536;
+        const t = (control.millis() / 100000) * 100;
+        const sz = storage.temporary.size(fn);
+        storage.temporary.appendLine(fn, `${t}> ${line}`);
+        // shring by 50% if too big
+        if (sz > 65536) {
+            const buf = storage.temporary.readAsBuffer(fn);
+            storage.temporary.overwriteWithBuffer(fn, buf.slice(buf.length / 2));
+        }
+    })
 }
