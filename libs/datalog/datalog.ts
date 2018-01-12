d8d089a33e3deaeb7e7e8cb8116617e0ad29e685
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-11 22:25:33 -0800

    simple datalog frameowrk

---
+++ libs/datalog/datalog.ts
@@ -0,0 +1,89 @@
+//% weight=100 color=#0fbc11 icon=""
+namespace datalog {
+    let _headers: string[];
+    let _headersLength: number;
+    let _values: number[];
+    let _start: number;
+    let _filename = "data.csv";
+    let _storage: storage.Storage = storage.temporary;
+
+    function clear() {
+        _headers = undefined;        
+        _values = undefined;
+    }
+
+    function init() {
+        if (!_headers) {
+            _headers = [];
+            _headersLength = 0;
+            _start = control.millis();
+            storage.temporary.remove(_filename);
+        }
+        _values = [];
+    }
+
+    function commit() {
+        // write row if any data
+        if (_values && _values.length > 0) {
+            // write headers for the first row
+            if (!_headersLength) {
+                storage.temporary.appendCSVHeaders(_filename, _headers);
+                _headersLength = _storage.size(_filename);
+            }
+            // commit row data
+            _storage.appendCSV(_filename, _values);
+        }
+
+        // clear values
+        _values = undefined;
+    }
+
+    /**
+     * Starts a row of data
+     */
+    //% weight=100
+    //% blockId=datalogAddRow block="datalog add row"
+    export function addRow(): void {
+        commit();
+        init();
+        addValue("time (ms)", control.millis() - _start);
+    }
+
+    /**
+     * Adds a cell to the row of data
+     * @param name name of the cell, eg: "x"
+     * @param value value of the cell, eg: 0
+     */
+    //% weight=99
+    //% blockId=datalogAddValue block="datalog add %name|=%value"
+    export function addValue(name: string, value: number) {
+        if (!_values) return;
+        let i = _headers.indexOf(name);
+        if (i < 0) {
+            _headers.push(name);
+            i = _headers.length - 1;
+        }
+        _values[i] = value;
+        if (i > 0) // 0 is time
+            console.logValue(name, value)
+    }
+
+    /**
+     * Starts a new data logger for the given file
+     */
+    //% blockId=datalogSetFile block="datalog set file %filename"
+    export function setFile(fn: string) {
+        _filename = fn;
+        clear();
+    }
+
+    /**
+     * 
+     * @param storage custom storage solution
+     */
+    //%
+    export function setStorage(storage: storage.Storage) {
+        _storage = storage;
+        clear();
+    }
+}
