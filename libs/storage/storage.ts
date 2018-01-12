cd608634acce6f9e8cb50b923cb3bd70e209584f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-11 16:08:42 -0800

    missing sim stubs

--- libs/storage/storage.ts
+++ libs/storage/storage.ts
@@ -3,7 +3,7 @@ namespace storage {
     console.addListener(function(line) {
         const fn = "console.txt";
         const mxs = 65536;
-        const t = (control.millis() / 100000) * 100;
+        const t = Math.round(control.millis() / 100000) * 100;
         const sz = storage.temporary.size(fn);
         storage.temporary.appendLine(fn, `${t}> ${line}`);
         // shring by 50% if too big
---
+++ sim/state/storage.ts
@@ -0,0 +1,15 @@
+namespace pxsim.storage {
+    export function __stringToBuffer(s: string): RefBuffer {
+        // TODO
+        return new RefBuffer([]);
+    }
+
+    export function __bufferToString(b: RefBuffer): string {
+        // TODO
+        return "";
+    }
+
+    export function __mkdir(fn: string) {
+        // TODO
+    }
+}
