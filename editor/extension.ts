73363d11b27c7608bf662e015a2baef6a65c2d5b
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-29 15:21:15 -0800

    Download dialog (#248)
    * download dialog
    
    * updated pxt reference
    
    * typo

--- editor/extension.ts
+++ editor/extension.ts
@@ -8,7 +8,7 @@ import { FieldSpeed } from "./field_speed";
 import { FieldBrickButtons } from "./field_brickbuttons";
 import { FieldTurnRatio } from "./field_turnratio";
 
-pxt.editor.initExtensionsAsync = function(opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
+pxt.editor.initExtensionsAsync = function (opts: pxt.editor.ExtensionOptions): Promise<pxt.editor.ExtensionResult> {
     pxt.debug('loading pxt-ev3 target extensions...')
     updateBlocklyShape();
     const res: pxt.editor.ExtensionResult = {
@@ -28,7 +28,95 @@ pxt.editor.initExtensionsAsync = function(opts: pxt.editor.ExtensionOptions): Pr
             selector: "turnratio",
             editor: FieldTurnRatio
         }],
-        deployCoreAsync
+        deployCoreAsync,
+        showUploadInstructionsAsync: (fn: string, url: string, confirmAsync: (options: any) => Promise<number>) => {
+            let resolve: (thenableOrResult?: void | PromiseLike<void>) => void;
+            let reject: (error: any) => void;
+            const deferred = new Promise<void>((res, rej) => {
+                resolve = res;
+                reject = rej;
+            });
+            const boardName = pxt.appTarget.appTheme.boardName || "???";
+            const boardDriveName = pxt.appTarget.appTheme.driveDisplayName || pxt.appTarget.compile.driveName || "???";
+
+            // https://msdn.microsoft.com/en-us/library/cc848897.aspx
+            // "For security reasons, data URIs are restricted to downloaded resources. 
+            // Data URIs cannot be used for navigation, for scripting, or to populate frame or iframe elements"
+            const downloadAgain = !pxt.BrowserUtils.isIE() && !pxt.BrowserUtils.isEdge();
+            const docUrl = pxt.appTarget.appTheme.usbDocs;
+            const saveAs = pxt.BrowserUtils.hasSaveAs();
+
+            const htmlBody = `
+            <div class="ui three column grid stackable">
+                <div class="column">
+                    <div class="ui">
+                        <div class="image">
+                            <img class="ui medium rounded image" src="./static/download/connect.svg" style="height:109px;width:261px;margin-bottom:1rem;">
+                        </div>
+                        <div class="content">
+                            <div class="description">
+                                <span class="ui yellow circular label">1</span>
+                                <strong>${lf("Connect EV3 to computer with USB cable")}</strong>
+                                <br/>
+                                ${lf("Use the miniUSB port on top of EV3 brick")}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui">
+                        <div class="image">
+                            <img class="ui medium rounded image" src="./static/download/firmware.svg" style="height:109px;width:261px;margin-bottom:1rem;">
+                        </div>
+                        <div class="content">
+                            <div class="description">
+                                <span class="ui blue circular label">2</span>
+                                <strong>${lf("Make sure you have the latest EV3 firmware")}</strong>
+                                <br/>
+                                <a href="" target="_blank">${lf("Click here to update to latest firmware")}</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="ui">
+                        <div class="image">
+                            <img class="ui medium rounded image" src="./static/download/transfer.svg" style="height:109px;width:261px;margin-bottom:1rem;">
+                        </div>
+                        <div class="content">
+                            <div class="description">
+                                <span class="ui blue circular label">3</span>
+                                ${lf("Move the .uf2 file to EV3 brick")}
+                                <br/>
+                                ${lf("Locate the downloaded .uf2 file and drag it to the EV3 drive")}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+
+            return confirmAsync({
+                header: lf("Download to your EV3"),
+                htmlBody,
+                hasCloseIcon: true,
+                hideCancel: true,
+                hideAgree: false,
+                agreeLbl: lf("I got it"),
+                buttons: [downloadAgain ? {
+                    label: fn,
+                    icon: "download",
+                    class: "lightgrey focused",
+                    url,
+                    fileName: fn
+                } : undefined, docUrl ? {
+                    label: lf("Help"),
+                    icon: "help",
+                    class: "lightgrey",
+                    url: docUrl
+                } : undefined]
+                //timeout: 20000
+            }).then(() => { });
+        }
     };
     initAsync().catch(e => {
         // probably no HID - we'll try this again upon deployment
