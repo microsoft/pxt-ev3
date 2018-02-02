a7fd19b79fe294ea2f20377b078293c7ed0c4961
Caitlin Hennessy <hennessy.caitlin@gmail.com>
2018-02-02 11:09:30 -0800

    Cleanup

--- sim/visuals/controls/colorWheel.ts
+++ sim/visuals/controls/colorWheel.ts
@@ -1,5 +1,3 @@
-
-
 namespace pxsim.visuals {
 
     export class ColorWheelControl extends ControlView<ColorSensorNode> {
@@ -7,9 +5,7 @@ namespace pxsim.visuals {
         private colorGradient: SVGLinearGradientElement;
         private defs: SVGDefsElement;
         private reporter: SVGTextElement;
-
         private rect: SVGElement;
-        private sliderGroup: SVGElement;
 
         getInnerWidth() {
             return 111;
@@ -32,7 +28,6 @@ namespace pxsim.visuals {
         }
 
         private getMax() {
-            // TODO
             return 100;
         }
 
@@ -49,8 +44,6 @@ namespace pxsim.visuals {
 
         updateColorLevel(pt: SVGPoint, parent: SVGSVGElement, ev: MouseEvent) {
             let cur = svg.cursorPoint(pt, parent, ev);
-            //const height = this.getSliderHeight();
-            //const bBox = this.content.getBoundingClientRect();
             const bBox = this.rect.getBoundingClientRect();
             const height = bBox.height;
             let t = Math.max(0, Math.min(1, (height + bBox.top / this.scaleFactor - cur.y / this.scaleFactor) / height));
@@ -61,7 +54,6 @@ namespace pxsim.visuals {
         getInnerView(parent: SVGSVGElement) {
             this.defs = <SVGDefsElement>svg.child(this.element, "defs", {});
             this.group = svg.elt("g") as SVGGElement;
-            //this.group.setAttribute("transform", `translate(12, 0) scale(2)`)
 
             let gc = "gradient-color";
             this.colorGradient = svg.linearGradient(this.defs, gc, false);
@@ -74,7 +66,6 @@ namespace pxsim.visuals {
 
             const sliderGroup = pxsim.svg.child(this.group, "g");
             sliderGroup.setAttribute("transform", `translate(${this.getWidth() / 2 - this.getSliderWidth() / 2}, ${this.getReporterHeight()})`);
-            this.sliderGroup = sliderGroup;
 
             const rect = pxsim.svg.child(sliderGroup, "rect",
                 {
@@ -85,7 +76,6 @@ namespace pxsim.visuals {
                     "style": `fill: url(#${gc})`
                 }
             )
-
             this.rect = rect;
 
             const lowThreshold = pxsim.svg.child(sliderGroup, "line",
@@ -132,67 +122,6 @@ namespace pxsim.visuals {
             })
 
             return this.group;
-
-            /**
-            const rect = pxsim.svg.child(this.group, "g");
-            const innerRect = pxsim.svg.child(rect, "rect",
-                {cursor: '-webkit-grab',
-                fill: `url(#${gc})`,
-                width: this.getInnerWidth(),
-                height: this.getInnerHeight()
-            });
-
-            innerRect.setAttribute("transform", `translate(12, 0) scale(2)`);
-
-            let pt = parent.createSVGPoint();
-            let captured = false;
-            touchEvents(rect,
-                ev => {
-                    if (captured && (ev as MouseEvent).clientX) {
-                        ev.preventDefault();
-                        this.setColor(pt, parent, ev as MouseEvent);
-                    }
-                },
-                ev => {
-                    captured = true;
-                    if ((ev as MouseEvent).clientX) {
-                        this.setColor(pt, parent, ev as MouseEvent);
-                    }
-                },
-                ev => {
-                    captured = false;
-                },
-                ev => {
-                    captured = false;
-                }
-            )
-            return this.group;
-            **/
         }
-
-        /*
-        updateState() {
-            if (!this.visible) {
-                return;
-            }
-            const node = this.state;
-            const percentage = node.getValue();
-            svg.setGradientValue(this.colorGradient, percentage + "%");
-        }
-
-        private setColor(pt: SVGPoint, parent: SVGSVGElement, ev: MouseEvent) {
-            const height = CONTROL_HEIGHT;
-            let cur = svg.cursorPoint(pt, parent, ev);
-            //let t = Math.max(0, Math.min(1, (width + this.left / this.scaleFactor - cur.x / this.scaleFactor) / width));
-            let t = Math.max(0, Math.min(1, (height + this.top/this.scaleFactor - cur.y/this.scaleFactor) / height));
-            const state = this.state;
-            state.setColor((1-t)*100);
-        }
-        **/
     }
-
 }
-
-// scaleFactor = 0-1
-// height = size of the element
-// top, left = x, y of top corner
