f6f972885271ddce7ab2e8dfa8446a628b9ef643
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-02-02 08:03:57 -0800

    tracking button state

--- sim/state/infrared.ts
+++ sim/state/infrared.ts
@@ -1,7 +1,46 @@
 /// <reference path="./sensor.ts"/>
 
 namespace pxsim {
+    export enum InfraredRemoteButton {
+        //% block="center beacon"
+        CenterBeacon = 0x01,
+        //% block="top left"
+        TopLeft = 0x02,
+        //% block="bottom left"
+        BottomLeft = 0x04,
+        //% block="top right"
+        TopRight = 0x08,
+        //% block="bottom right"
+        BottomRight = 0x10,
+    }    
+
     export class RemoteState {
+        state: number = 0;
+
+        constructor() {
+        }
+
+        unmapButtons() {
+            switch(this.state) {
+                case InfraredRemoteButton.TopLeft: return 1;
+                case InfraredRemoteButton.BottomLeft: return 2;
+                case InfraredRemoteButton.TopLeft: return 3;
+                case InfraredRemoteButton.TopRight | InfraredRemoteButton.BottomRight: return 4;
+                case InfraredRemoteButton.TopLeft | InfraredRemoteButton.TopRight: return 5;
+                case InfraredRemoteButton.TopLeft | InfraredRemoteButton.BottomRight: return 6;
+                case InfraredRemoteButton.BottomLeft | InfraredRemoteButton.TopRight: return 7;
+                case InfraredRemoteButton.BottomLeft | InfraredRemoteButton.BottomRight: return 8;
+                case InfraredRemoteButton.CenterBeacon: return 9;
+                case InfraredRemoteButton.BottomLeft | InfraredRemoteButton.TopLeft: return 10;
+                case InfraredRemoteButton.TopRight | InfraredRemoteButton.BottomRight: return 11;
+                default: return 0;
+            }
+        }    
+
+        setPressed(btns: InfraredRemoteButton, down: boolean) {
+            if (down) this.state = this.state | btns;
+            else this.state = ~(~this.state | btns);
+        }
     }
 
     export enum InfraredSensorMode {
--- sim/visuals/controls/remoteBeaconButtons.ts
+++ sim/visuals/controls/remoteBeaconButtons.ts
@@ -16,7 +16,7 @@ namespace pxsim.visuals {
             this.group = svg.elt("g") as SVGGElement;
             this.group.setAttribute("transform", `translate(2, 2.5) scale(0.6)`)
 
-            const colorIds = [
+            const btnIds = [
                 InfraredRemoteButton.CenterBeacon, 
                 InfraredRemoteButton.TopLeft, 
                 InfraredRemoteButton.TopRight,
@@ -25,16 +25,23 @@ namespace pxsim.visuals {
             const colors = ['#f12a21', '#ffd01b', '#006db3', '#00934b', '#6c2d00'];
 
             let cy = -4;
-            for (let c = 0; c < colorIds.length; c++) {
+            btnIds.forEach((cid, c) => {
                 const cx = c % 2 == 0 ? 2.2 : 8.2;
                 if (c % 2 == 0) cy += 5;
-                if (colorIds[c]) {
+                if (btnIds[c]) {
                     const circle = pxsim.svg.child(this.group, "circle", { 'class': 'sim-color-grid-circle', 'cx': cx, 'cy': cy, 'r': '2', 'style': `fill: ${colors[c]}` });
                     pointerEvents.down.forEach(evid => circle.addEventListener(evid, ev => {
-                        // TODO
+                        ev3board().remoteState.setPressed(cid, true);
                     }));
+                    circle.addEventListener(pointerEvents.leave, ev => {
+                        ev3board().remoteState.setPressed(cid, false);
+                    });
+                    circle.addEventListener(pointerEvents.up, ev => {
+                        ev3board().remoteState.setPressed(cid, true);
+                    });
                 }
-            }
+
+            })
 
             return this.group;
         }
