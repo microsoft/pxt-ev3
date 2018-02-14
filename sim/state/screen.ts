e01ee20e5bad01c60b1d089480d6830db099c90a
Michal Moskal <michal@moskal.me>
2018-02-13 17:45:42 -0800

    Build fixes

--- sim/state/screen.ts
+++ sim/state/screen.ts
@@ -1,5 +1,14 @@
-
 namespace pxsim {
+    export enum Draw {
+        Normal = 0x00, // set pixels to black, no fill
+        Clear = 0x01,
+        Xor = 0x02,
+        Fill = 0x04,
+        Transparent = 0x08,
+        Double = 0x10,
+        Quad = 0x20,
+    }
+
 
     function OFF(x: number, y: number) {
         return x + y * visuals.SCREEN_WIDTH
