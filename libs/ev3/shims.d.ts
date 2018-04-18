5c432dd7f4c2329349880eef4a92e600909def3d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-18 08:33:09 -0700

    moving png.cpp into ev3

---
+++ libs/ev3/shims.d.ts
@@ -0,0 +1,9 @@
+// Auto-generated. Do not edit.
+declare namespace image {
+
+    /** Decompresses a 1-bit gray scale PNG image to image format. */
+    //% shim=image::unpackPNG
+    function unpackPNG(png: Buffer): Image;
+}
+
+// Auto-generated. Do not edit. Really.
