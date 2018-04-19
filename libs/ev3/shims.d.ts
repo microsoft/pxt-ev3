c44af5f62ebf6b0ba4d9fe7bd26397a2ef5069ab
Michal Moskal <michal@moskal.me>
2018-04-19 11:35:04 -0700

    Remove PNG support

--- libs/ev3/shims.d.ts
+++ libs/ev3/shims.d.ts
@@ -2,11 +2,5 @@
 
 
 declare interface Image {}
-declare namespace image {
-
-    /** Decompresses a 1-bit gray scale PNG image to image format. */
-    //% shim=image::unpackPNG
-    function unpackPNG(png: Buffer): Image;
-}
 
 // Auto-generated. Do not edit. Really.
