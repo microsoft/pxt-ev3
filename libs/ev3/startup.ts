bf300fdb5ae89bed85f666bfff1583b3cdfdae66
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-20 09:40:55 -0700

    set light, play sound, show eyes, wait, clear screen, light

--- libs/ev3/startup.ts
+++ libs/ev3/startup.ts
@@ -2,3 +2,10 @@
 
 // We pause for 100ms to give time to read sensor values, so they work in on_start block
 pause(100)
+
+// pulse green, play startup sound, turn off light
+brick.setStatusLight(StatusLight.GreenPulse);
+music.playSoundEffect(sounds.systemStartUp);
+brick.showImage(images.eyesAwake, 600);
+brick.setStatusLight(StatusLight.Off);
+brick.clearScreen();
