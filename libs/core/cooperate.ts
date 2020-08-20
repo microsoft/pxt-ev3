a7869c2b72e352a3f309b21fecbb575890e0b8d0
Peli de Halleux <peli@DESKTOP-5B7QRAM.corp.microsoft.com>
2020-08-20 21:06:29 +0200

    fix math

--- libs/core/cooperate.ts
+++ libs/core/cooperate.ts
@@ -3,7 +3,7 @@ namespace control {
     const COOPERATION_INTERVAL = 50
     export function cooperate() {
         const now = control.millis()
-        if (lastPause - now > COOPERATION_INTERVAL) {
+        if (now - lastPause > COOPERATION_INTERVAL) {
             pause(1)
             lastPause = now
         }
