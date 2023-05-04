cb56e09f9422107844d2985073cff15ae5bb3d64
Dmitriy Antipov <aston_bonny@mail.ru>
2023-05-04 15:50:02 +0300

    Update ns.ts
    Added option to select emptiness (nothing) for the colorEnumPicker block.

--- libs/color-sensor/ns.ts
+++ libs/color-sensor/ns.ts
@@ -1,5 +1,3 @@
-
-
 namespace sensors {
 
     /**
@@ -9,9 +7,9 @@ namespace sensors {
     //% blockId=colorEnumPicker block="%color" shim=TD_ID
     //% weight=0 blockHidden=1 turnRatio.fieldOptions.decompileLiterals=1
     //% color.fieldEditor="colorenum"
-    //% color.fieldOptions.colours='["#f12a21", "#ffd01b", "#006db3", "#00934b", "#000000", "#6c2d00", "#ffffff"]'
+    //% color.fieldOptions.colours='["#f12a21", "#ffd01b", "#006db3", "#00934b", "#000000", "#6c2d00", "#ffffff", "#dfe6e9"]'
     //% color.fieldOptions.columns=2 color.fieldOptions.className='legoColorPicker'
     export function __colorEnumPicker(color: ColorSensorColor): number {
         return color;
     }
-}
+}                       
