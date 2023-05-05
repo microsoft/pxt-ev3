e197e932d45076e64a2568d9ecbb6d26bb4c22cd
Dmitriy Antipov <aston_bonny@mail.ru>
2023-05-06 01:39:50 +0300

    Update libs/screen/targetoverrides.ts
    Co-authored-by: Joey Wunderlich <jwunderl@users.noreply.github.com>

--- libs/screen/targetoverrides.ts
+++ libs/screen/targetoverrides.ts
@@ -165,7 +165,7 @@ namespace brick {
      * @param column the column number to print the text at (starting at 1), eg: 1
      * @param printStyle print style black on white or white on black
      */
-    //% blockId=screenPrintValue block="show value %name|= %text|at line %line||and column %column|with style %printStyle"
+    //% blockId=screenPrintValue block="show value $name|= $value|at line $line||column $column|style $printStyle"
     //% weight=96 group="Screen" inlineInputMode="inline" blockGap=8
     //% expandableArgumentMode="enabled"
     //% help=brick/show-value
