944841f544d86b0446a2640b3464d9b72d368570
Dmitriy Antipov <aston_bonny@mail.ru>
2023-07-18 07:46:52 +0300

    Update ns.ts

--- libs/ev3/ns.ts
+++ libs/ev3/ns.ts
@@ -24,6 +24,11 @@ namespace chassis {
 
 }
 
+//% labelLineWidth=0
+namespace behaviors {
+
+}
+
 //% color="#D67923" weight=80 icon="\uf10e"
 //% groups='["Sounds", "Tone", "Volume", "Tempo"]'
 namespace music {
