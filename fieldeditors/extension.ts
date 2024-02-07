118e7af5056fb4414a03e1ebadce8449958b4101
Dmitriy Antipov <aston_bonny@mail.ru>
2024-02-07 15:30:47 +0300

    consume-pxteditor-changes-and-upgrade-code

--- fieldeditors/extension.ts
+++ fieldeditors/extension.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/pxt-core/built/pxteditor.d.ts"/>
+/// <reference path="../node_modules/pxt-core/localtypings/pxteditor.d.ts"/>
 /// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
 
 import { FieldPorts } from "./field_ports";
