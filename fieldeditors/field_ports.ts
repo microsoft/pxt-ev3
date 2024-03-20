e67e6687029c80146d6281ab9775952cef9c4883
Dmitriy Antipov <aston_bonny@mail.ru>
2024-03-20 11:34:26 +0300

    Update field_ports.ts

--- fieldeditors/field_ports.ts
+++ fieldeditors/field_ports.ts
@@ -17,7 +17,6 @@ export class FieldPorts extends pxtblockly.FieldImages {
         this.columns_ = parseInt(options.columns) || 4;
         this.width_ = parseInt(options.width) || 300;
 
-        //this.setText = Blockly.FieldDropdown.prototype.setText;
         this.updateSize_ = (Blockly.Field as any).prototype.updateSize_;
     }
     
